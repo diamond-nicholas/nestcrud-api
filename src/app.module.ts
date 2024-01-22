54e036925882c39821400b3250a05ed659f07f09
diamond nicholas <diamondnicholas154@gmail.com>
2024-01-22 23:34:55 +0000

    complete crud sample

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [DatabaseModule, ProductsModule],
+  imports: [DatabaseModule, ProductsModule, PaymentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,19 +10,22 @@ export class ProductsService {
     return this.databaseService.product.create({ data: createProductDto });
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return this.databaseService.product.findMany({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    return this.databaseService.product.findUnique({ where: { id } });
   }
 
-  update(id: number, updateProductDto: Prisma.ProductUpdateInput) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: Prisma.ProductUpdateInput) {
+    return this.databaseService.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    return this.databaseService.product.delete({ where: { id } });
   }
 }
